1c39e7060e67e97c2824b59266f233e98dc9ae4b
Zander Schwid & Co <zander@schwid.com>
2023-09-25 21:55:05 -0700

    update scanner

--- scan.go
+++ scan.go
@@ -6,31 +6,11 @@
 package dnsmod
 
 import (
-	"github.com/codeallergy/glue"
-	"github.com/sprintframework/dns"
 	"github.com/sprintframework/dnsmod/netlify"
 )
 
-type dnsScanner struct {
-	Scan     []interface{}
-}
-
-func Scanner(scan... interface{}) glue.Scanner {
-	return &dnsScanner {
-		Scan: scan,
-	}
-}
-
-func (t *dnsScanner) Beans() []interface{} {
-
-	beans := []interface{}{
-		WhoisService(),
-		netlify.NetlifyProvider(),
-		&struct {
-			DNSProviders []dns.DNSProvider `inject`
-		}{},
-	}
-
-	return append(beans, t.Scan...)
+var DNSServices = []interface{} {
+	WhoisService(),
+	netlify.NetlifyProvider(),
 }
 
